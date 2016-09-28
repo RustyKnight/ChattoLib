3f72c71d4a0ce4fa6bffa8d4b6aabcdbd432f8ad
Shane Whitehead <shane.whitehead@beamcommunications.com>
2016-09-28 15:42:49 +1000

    Initial commit

---
+++ ChattoLib/ChattoLib.h
@@ -0,0 +1,22 @@
+//
+//  ChattoLib.h
+//  ChattoLib
+//
+//  Created by Shane Whitehead on 28/09/2016.
+//  Copyright © 2016 Beam Communications. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ChattoLib.
+FOUNDATION_EXPORT double ChattoLibVersionNumber;
+
+//! Project version string for ChattoLib.
+FOUNDATION_EXPORT const unsigned char ChattoLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChattoLib/PublicHeader.h>
+
+#import "CircleProgressIndicatorView.h"
+#import "CircleProgressView.h"
+
+
